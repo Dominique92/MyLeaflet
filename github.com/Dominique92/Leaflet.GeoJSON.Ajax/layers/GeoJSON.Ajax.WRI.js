b2f70e458c3682748f2df110e8538ad0f0c4b8cb
Dominique Cavailhez <protec@cavailhez.fr>
2016-08-20 11:18:52 +0200

    Draw demo update

--- github.com/Dominique92/Leaflet.GeoJSON.Ajax/layers/GeoJSON.Ajax.WRI.js
+++ github.com/Dominique92/Leaflet.GeoJSON.Ajax/layers/GeoJSON.Ajax.WRI.js
@@ -57,14 +57,6 @@ L.GeoJSON.Ajax.chemineur = L.GeoJSON.Ajax.extend({
 			site: 'chemineur,camptocamp,pyrenees-refuges,refuges.info',
 			poi: '3,8,16,20,23,28,30,40,44,58,62,64'
 		},
-		bbox: true,
-		wstyle: function(feature) {
-			return {
-				popup: feature.properties.nom,
-				url: feature.properties.lien,
-				color: feature.properties.couleur,
-				weight: 2
-			};
-		}
+		bbox: true
 	}
 });
