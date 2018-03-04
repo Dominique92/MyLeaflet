8903f4e8bbd1a9acb998066464d68707ecee7bff
Dominique Cavailhez <protec@cavailhez.fr>
2018-03-04 20:08:10 +0100

    Misc

--- src/AntiBot.js
+++ src/AntiBot.js
@@ -3,5 +3,5 @@
  * N'active Leaflet pas si l'agent est un robot
  */
 
-if (navigator.userAgent.search (/arach|archiver|bot|crawl|curl|factory|index|partner|rss|seek|search|semantic|scoot|spider|spyder|yandex/i) != -1)
+if (navigator.userAgent.search (/arach|archiver|bot|crawl|curl|factory|googlebot|index|partner|robot|rss|scoot|search|seek|semantic|spider|spyder|yandex/i) != -1)
 	L.Map.prototype.addLayer = function () {}
